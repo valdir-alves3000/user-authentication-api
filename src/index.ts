e80d1b3069ba40e17447695446f88b7abed371ba
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2022-02-03 10:42:29 -0300

    Finalizing concept of routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
+import usersRoute from "./routes/users.route";
 
 const app = express();
 
-app.get("/status", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send([{ foo: "bar" }]);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(usersRoute);
 
 app.listen(3000, () => {
   console.log("Aplicação executando na porta 3000!");
